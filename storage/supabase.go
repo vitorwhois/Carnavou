2464fc020c2ce2f3625f3d429c4134affac9b895
Vitor Ruis da Silva <Vitor@MacBook-Pro-de-Mariana-2.local>
2024-01-29 20:55:17 -0300

    tentativa de depois

--- storage/supabase.go
+++ storage/supabase.go
@@ -4,7 +4,7 @@ import (
 	"database/sql"
 	"fmt"
 	"log"
-//	"os"
+	"os"
 
 //	"github.com/joho/godotenv"
 	_ "github.com/lib/pq"
@@ -18,31 +18,21 @@ var SupabaseDB *sql.DB
 
 func init() {
 	// Retirar para fazer o deploy no Render
-	/*
-	if err := godotenv.Load(); err != nil {
-		log.Fatal("Erro ao carregar as variáveis de ambiente:", err)
-	}
+	
+//	if err := godotenv.Load(); err != nil {
+//		log.Fatal("Erro ao carregar as variáveis de ambiente:", err)
+//	}
 
 	supabaseUrl = os.Getenv("SUPABASE_URL")
 	supabaseKey = os.Getenv("SUPABASE_KEY")
 	postgresHost = os.Getenv("POSTGRES_HOST")
 	postgresPassword = os.Getenv("POSTGRES_PASSWORD")
-	
-
 	db, err := sql.Open("postgres", fmt.Sprintf("user=postgres password=%s host=%s port=5432 dbname=postgres", postgresPassword, postgresHost))
 	if err != nil {
 		log.Fatal(err)
 	}
-	*/
-	db, err := sql.Open("postgres", fmt.Sprintf("user=postgres password=POSTGRES_PASSWORD host=POSTGRES_HOST port=5432 dbname=postgres"))
-	if err != nil {
-		log.Fatal(err)
-	}
-
-
 	SupabaseDB = db
 }
-
 func OpenDatabaseConnection() (*sql.DB, error) {
 	// Função para abrir a conexão com o banco de dados
 	// Retornará um ponteiro para o DB para ser usado em outras partes do código
