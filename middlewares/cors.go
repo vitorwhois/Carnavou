b24726e29c6ecabfcb131c8454c3f073507a5aab
Vitor Ruis <108823068+vitorwhois@users.noreply.github.com>
2024-12-10 20:11:19 -0200

    fix: connection envs

--- middlewares/cors.go
+++ middlewares/cors.go
@@ -2,19 +2,32 @@ package middlewares
 
 import "net/http"
 
-// Middleware para configurar CORS
 func EnableCORS(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		// Configuração de cabeçalhos CORS
-		w.Header().Set("Access-Control-Allow-Origin", "*") // Ajuste conforme necessário
+
+		allowedOrigins := []string{
+			"https://carnavou.onrender.com",
+			"http://localhost:3000",
+			"http://localhost:8080",
+		}
+
+		origin := r.Header.Get("Origin")
+		for _, o := range allowedOrigins {
+			if o == origin {
+				w.Header().Set("Access-Control-Allow-Origin", origin)
+				break
+			}
+		}
+
 		w.Header().Set("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS")
+
 		w.Header().Set("Access-Control-Allow-Headers", "Content-Type, Authorization")
 
-		// Responder pré-via (preflight) se o método for OPTIONS
 		if r.Method == http.MethodOptions {
 			w.WriteHeader(http.StatusOK)
 			return
 		}
+
 		next.ServeHTTP(w, r)
 	})
 }
